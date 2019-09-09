91d8872f876fcfc8ec4b7fbab0dddc7ee3a0b8cc
HelloWmz <1170155848@qq.com>
2019-09-09 16:20:07 +0800

    test

---
+++ app/src/main/java/com/example/gitdeam/TextActivity.java
@@ -0,0 +1,13 @@
+package com.example.gitdeam;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TextActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
