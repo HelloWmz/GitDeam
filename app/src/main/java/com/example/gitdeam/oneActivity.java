62f0d1516e3d0ecb0acb1805992ad3572b38f3f3
HelloWmz <1170155848@qq.com>
2019-09-09 17:06:17 +0800

    修改名称
    修改名称
    wp
    pw
    git commit  -m 修改名称

--- app/src/main/java/com/example/gitdeam/oneActivity.java
+++ app/src/main/java/com/example/gitdeam/oneActivity.java
@@ -3,6 +3,7 @@ package com.example.gitdeam;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class TextActivity extends AppCompatActivity {
     @Override
@@ -11,6 +12,6 @@ public class TextActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
         //todo
         //你是谁的的啦
-     
+        Toast.makeText(this,"你是不是"，Toast.LENGTH_LONG)
     }
 }
