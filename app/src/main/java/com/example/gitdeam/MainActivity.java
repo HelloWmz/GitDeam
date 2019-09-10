176baf95b8c34644d920ef26f8869fe5d6236f03
HelloWmz <1170155848@qq.com>
2019-09-10 08:35:38 +0800

    提交完成成

--- app/src/main/java/com/example/gitdeam/MainActivity.java
+++ app/src/main/java/com/example/gitdeam/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.gitdeam;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTextActivity(View view) {
-      startActivity(new Intent(this,TextActivity.class));
+      startActivity(new Intent(this, oneActivity.class));
+        Toast.makeText(this,"你是不是",Toast.LENGTH_LONG);
     }
 }
--- app/src/main/java/com/example/gitdeam/oneActivity.java
+++ app/src/main/java/com/example/gitdeam/oneActivity.java
@@ -5,13 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class TextActivity extends AppCompatActivity {
+public class oneActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         //todo
         //你是谁的的啦
-        Toast.makeText(this,"你是不是"，Toast.LENGTH_LONG)
+        Toast.makeText(this,"你是不是",Toast.LENGTH_LONG);
     }
 }
