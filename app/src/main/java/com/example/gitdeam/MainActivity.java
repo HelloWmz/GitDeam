f493e294bc1e9b5c80fa4929056fb7117ca6a49e
wangmingzhen <1170155848@qq.com>
2020-11-11 12:11:05 +0800

    222

--- app/src/main/java/com/example/gitdeam/MainActivity.java
+++ app/src/main/java/com/example/gitdeam/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"sadadsada",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"你是不是",Toast.LENGTH_LONG).show();
         Toast.makeText(this,"你是不是",Toast.LENGTH_LONG).show();
+        Log.e("test","new new2222wenwewe");
         Log.e("test","new new wenwewe");
     }
 }
